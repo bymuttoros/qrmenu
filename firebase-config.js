02e435c178ab75b9a59c04e130950792361cbcd4
bymuttoros <muttoros@gmail.com>
2026-09-09 11:51:29 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCtj0lgnGjLbpW51Ttioo4gxMWSVZw16VM",
+  authDomain: "cafepark-d1ebf.firebaseapp.com",
+  databaseURL: "https://cafepark-d1ebf-default-rtdb.firebaseio.com",
+  projectId: "cafepark-d1ebf",
+  storageBucket: "cafepark-d1ebf.firebasestorage.app",
+  messagingSenderId: "649792859092",
+  appId: "1:649792859092:web:44b82ddc55d042a7134441"
+};
